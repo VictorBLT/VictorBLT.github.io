2d005c0a4fe52bc489b1fa4d58c653682c5fac12
Victor <victor.blot@gmail.com>
2026-09-03 01:53:20 +0200

    feat: replaces double slashes by logo

--- src/components/title/Title.jsx
+++ src/components/title/Title.jsx
@@ -1,10 +1,12 @@
 import './title.css';
 
+import slash from '../../../public/icons/slash.svg';
+
 export default function Title({ children }) {
     return (
-        <h1>
-            <span className="accent">{'//'}</span>
-            {children}
-        </h1>
+        <div className="title">
+            <img src={slash} alt="logo" draggable={false} width={30} height={30} />
+            <h1>{children}</h1>
+        </div>
     );
 }
--- src/pages/resume/components/PDFSection.jsx
+++ src/pages/resume/components/PDFSection.jsx
@@ -1,19 +1,20 @@
-import { StyleSheet, Text, View } from '@react-pdf/renderer';
+import { Image, StyleSheet, Text, View } from '@react-pdf/renderer';
 
 import { styles, theme } from '../ResumeStyle.jsx';
 
 const localStyles = StyleSheet.create({
     title: {
         flexDirection: 'row',
-        alignItems: 'left',
-        gap: 6,
+        alignItems: 'center',
+        gap: 4,
         paddingBottom: 3,
         borderBottomWidth: 1,
         borderBottomColor: theme.colors.blue3,
         marginBottom: 8,
     },
     slash: {
-        color: theme.colors.accent,
+        width: 16,
+        height: 16,
     },
     container: {
         gap: 5,
@@ -21,10 +22,12 @@ const localStyles = StyleSheet.create({
 });
 
 export default function PDFSection({ title, children }) {
+    const baseUrl = import.meta.env.BASE_URL;
+
     return (
         <View>
             <View style={localStyles.title}>
-                <Text style={[styles.h1, localStyles.slash]}>{`//`}</Text>
+                <Image style={[localStyles.slash]} src={`${baseUrl}icons/slash.svg`} />
                 <Text style={styles.h1}>{title}</Text>
             </View>
             <View style={localStyles.container}>{children}</View>
