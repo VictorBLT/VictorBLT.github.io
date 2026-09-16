33da2b26d3d8494773d7972c12b0bb811b3ae778
Victor <victor.blot@gmail.com>
2026-09-16 02:54:14 +0200

    feat: Add dropdown arrow to LanguageSelector"

--- src/components/selector/LanguageSelector.jsx
+++ src/components/selector/LanguageSelector.jsx
@@ -41,6 +41,19 @@ export default function LanguageSelector() {
                     }}
                 />
                 <span>{currentLang.label}</span>
+                <svg
+                    className={`dropdown-arrow ${isOpen ? 'open' : ''}`}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round">
+                    <polyline points="6 9 12 15 18 9"></polyline>
+                </svg>
             </button>
 
             {isOpen && (
